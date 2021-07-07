b4ff807eb301f0f6e0c4900df20060145eed492c
mairis <mairiscastroflorez@gmail.com>
2021-07-06 21:14:09 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import validator from './validator.js';
 console.log(validator);
 
+/*En adelante se observan las funciones que dentro del botón 'Validar' permiten el despliegue de la vetana
+modal con los resultados, en la que se aplican los metodos Maskify e isValid contenidos en el 
+objeto Validator en el archivo Validator.js, importado a este documento en las primeras lineas del codigo */
 
 const btnR = document.querySelector("#btnModalR");
  btnR.addEventListener('click', () => {
 let cNum = document.getElementById('cNumero').value;
-const masked = validator.maskify(cNum);
+const masked = validator.maskify(cNum); //linea 10 y 11 se declaran las variables constantes que guardan los metodos isValid y Maskify
 const isValid = validator.isValid(cNum);
 if (cNum === ''|| cNum.length > 18) {
-	window.alert('Ingresa un numero de tarjeta valido');
+	window.alert('Ingresa un numero de tarjeta valido'); //la propiedad especificada en la linea 12 y 13 dictamina que en caso de que
+	//el valor recibido en cNum (variable que guarda el numero de tarjeta) sea vacío o mayor a 18, entonces se emitirá una alerta de rechazo
 
-} else if (document.getElementById("btnModalR")){
+} else if (document.getElementById("btnModalR")){ //a partir de esta linea y hasta la linea se despliega la ventana modal con los resultados
 			let modalR = document.getElementById("tvesModalR");
 			let spanR = document.getElementsByClassName("closeR")[0];
 			let bodyR = document.getElementsByTagName("body")[0];
@@ -22,7 +26,7 @@ if (cNum === ''|| cNum.length > 18) {
 				bodyR.style.height = "100%";
 				bodyR.style.overflow = "hidden";
 
-				if (isValid === true) {
+				if (isValid === true) { //se efectuan los resultados aplicando isValid y Maskify, metodos que validan el numero de tarjeta y lo ocultan
 				document.getElementById('Result').innerHTML = 'El numero '+masked+ ' es Valido. Es legal usar tu tarjeta. ';
 				} else {
 					document.getElementById('Result').innerHTML = 'El numero '+masked+ ' falso. No es legar usar tu tarjeta. ';
@@ -54,32 +58,7 @@ if (cNum === ''|| cNum.length > 18) {
   
  ); 
 
- /*
-const $icono = document.querySelector('#icono');
-$icono.addEventListener('click', () => {
-    $primeraPagina.style.display = 'block';
-    $segundaPag.style.display = 'none';
-    document.getElementById('cNumero').value = '';
-}
-);
-*/
-
-// este boton que se encuentra en la segunda pagina redirecciona, luego de mostrar el valor
-//a la segunda primera pagina, o pagina de inicio
-/*
-const $btnInicio = document.querySelector('#btnInicio');
-
-
-$btnInicio.addEventListener('click', () => {
-    $primeraPagina.style.display = 'block';
-    $segundaPag.style.display = 'none';
-    document.getElementById('cNumero').value = '';
-}
-);
-*/
-
-//ventanas modal que muestra la información contenida en dos botones en la pagina de inicio
-
+ //el condicional a continuación usando metodos parecidos al anterior ya visto, despliega la ventana modal de 'Muestrame como hacerlo
 if(document.getElementById("btnModal")){
 			var modal = document.getElementById("tvesModal");
 			var btn = document.getElementById("btnModal");
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,8 @@
 
 const validator = {
-
+//tambíen llamado algoritmo del modulo 10, el algoritmo de Luhn que valida distan convinaciones de numeros
+//entre ellos tarjetas de creditos operado en el metodo isValid, con el uso de operadores ternarios y condicionales
+//opera el mecanismo de validación de este programa recibiendo primero el valor de la tarjeta de credito y sometiendolo a distintas operaciones
 isValid(creditCardNumber){
 let converseDig = (creditCardNumber).split('').reverse();
 let parDig = 0;
@@ -21,6 +23,8 @@ let sumatoria = 0;
  return sumatoria %10 ===0 ? true: false
 },
 
+//el metodo maskify oculta todos los primeros valores del valor recibido de la tarjeta de credito mientras deja los ultimos cuatro siempre
+//visibles. ej: 182938382283 = ########2283, 1234 = 1234.
 
 maskify(creditCardNumber) {
 return creditCardNumber.slice(0, -4).replace(/./g, '#') + creditCardNumber.slice(-4);
@@ -30,7 +34,7 @@ return creditCardNumber.slice(0, -4).replace(/./g, '#') + creditCardNumber.slice
 
 
 };
-
+//exportando objeto validator para ser recibido en el archivo index.js
 export default validator;
 
 
